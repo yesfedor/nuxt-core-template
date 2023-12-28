163dc3257c7ac89ade009d79d8db307580dc595a
fedor <f.garanin@itspecial.net>
2023-12-28 23:44:57 +0400

    v3.9.14

--- pages/index.vue
+++ pages/index.vue
@@ -5,7 +5,7 @@
     </h1>
     <nuxt-icon name="reorder" filled />
     <p>This will be merged into the master.</p>
-    <p>This is production: Nuxt Core Template v3.9.0 with dockerized v2</p>
+    <p>This is production: Nuxt Core Template v3.9.0 with dockerized v3</p>
   </div>
 </template>
 
