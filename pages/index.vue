e4cfc2aca966bdfaf1abf7b65998f6b4709fc325
fedor <f.garanin@itspecial.net>
2024-01-01 20:32:32 +0400

    v3.9.0

--- pages/index.vue
+++ pages/index.vue
@@ -4,7 +4,6 @@
       {{ $t('welcome') }}
     </h1>
     <nuxt-icon name="reorder" filled />
-    <p>This will be merged into the master.</p>
     <p>This is production: Nuxt Core Template v3.9.0</p>
     <p>Host: {{ baseUrl }}</p>
   </div>
