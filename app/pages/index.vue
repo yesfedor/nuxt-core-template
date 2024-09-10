2e00c4e9096ffdeb6aad759dff83870e8af4e296
yesfedor <yesfedor.go@gmail.com>
2024-09-11 02:31:25 +0300

    Update Nuxt 3.13.1 and improve eslint

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -1,46 +1,53 @@
-<template>
-	<div class="app-page-home">
-		<h1 class="app-page-home__title">
-			{{ $t('welcome') }}
-		</h1>
-
-		<ui-icon name="reorder" filled />
-
-		<p>This is production: Nuxt Core Template v3.13.1</p>
-
-		<p>Host: {{ baseUrl }}</p>
-
-		<template v-if="first?.id">
-			<p>Your unique cat</p>
-			<img :src="first.url" :alt="first.id" class="app-page-home__cat">
-		</template>
-	</div>
-</template>
-
 <script lang="ts" setup>
-import { useCatCatalog } from '~~/layers/catapi/composables/useCatCatalog'
+const nuxtApp = useNuxtApp()
 
-const config = useRuntimeConfig()
-const baseUrl = ref(config.public.BASE_URL)
+const baseUrl = ref(nuxtApp.$config.public.BASE_URL)
+const nuxtVersion = ref(nuxtApp.versions.nuxt)
 
 // example use auto import api folder
 const { first } = await useCatCatalog()
 </script>
 
+<template>
+  <div class="app-page-home">
+    <h1 class="app-page-home__title">
+      {{ $t('welcome') }}
+    </h1>
+
+    <ui-icon
+      name="reorder"
+      filled
+    />
+
+    <p>This is production: Nuxt Core Template v{{ nuxtVersion }}</p>
+
+    <p>Host: {{ baseUrl }}</p>
+
+    <template v-if="first?.id">
+      <p>Your unique cat</p>
+      <img
+        :src="first.url"
+        :alt="first.id"
+        class="app-page-home__cat"
+      >
+    </template>
+  </div>
+</template>
+
 <style lang="scss">
 .app-page-home {
-	display: flex;
-	align-items: center;
-	flex-direction: column;
+  display: flex;
+  align-items: center;
+  flex-direction: column;
 
-	&__title {
-		color: #00dc82;
-	}
+  &__title {
+    color: #00dc82;
+  }
 
-	&__cat {
-		max-width: 50vw;
-		max-height: 50vh;
-		object-fit: cover;
-	}
+  &__cat {
+    max-width: 50vw;
+    max-height: 50vh;
+    object-fit: cover;
+  }
 }
 </style>
